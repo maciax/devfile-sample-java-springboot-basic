a67a3048d4e8dde65b8f3287f5ebfb78a750cd1d
Alberto Macias <maciax@gmail.com>
2022-12-13 15:19:29 -0600

    Cambios

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Cambios de ULA!";
+        return "OpenShift Cambio desde repositorio!";
     }
 
     public static void main(String[] args) {
